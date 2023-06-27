616997f40319bc4f771de8b25c9280fcb9dad6fe
Corey Hayden <cghayden@gmail.com>
2023-06-26 21:53:22 -0400

    fonts and favicon

--- app/root.tsx
+++ app/root.tsx
@@ -19,12 +19,17 @@ export default function App() {
       <head>
         <meta charSet='utf-8' />
         <meta name='viewport' content='width=device-width,initial-scale=1' />
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <style>
+          @import
+          url('https://fonts.googleapis.com/css2?family=Hind+Siliguri:wght@300;400;500&family=Philosopher&family=Plus+Jakarta+Sans:wght@300;400;500;600&family=Urbanist:wght@300;400;500;600&family=Ysabeau:wght@1;100;200;300;400;500;600&display=swap');
+        </style>
         <Meta />
         <Links />
       </head>
       <body
         className='bg-lime-950 
-       text-yellow-100 p-4'
+       text-yellow-100 p-4 font-ysabeau'
       >
         <Header />
         <Outlet />
