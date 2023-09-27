fb5a2d4b1383b555bfac4795d8bd16d6b8876c52
Corey Hayden <cghayden@gmail.com>
2023-09-27 14:13:22 -0400

    temporary header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,8 +3,9 @@ import SocialLinks from './SocialLinks'
 
 export default function Header() {
   return (
-    <nav className='md:flex items-center pt-5 md:p-7 '>
-      <div className='flex flex-col gap-2 items-center md:items-start '>
+    <nav className='md:flex items-center justify-center pt-5 md:p-7 '>
+      <div className='flex flex-col gap-2 items-center '>
+        {/* <div className='flex flex-col gap-2 items-center md:items-start '> */}
         <Link to='/'>
           <h1 className='text-4xl text-center'>Corey Hayden</h1>
         </Link>
@@ -13,20 +14,20 @@ export default function Header() {
           <SocialLinks linkedInHandle={'corey-hayden'} />
         </div>
       </div>
-      <ul className='flex justify-around ml-auto py-4 md:py-0'>
-        {/* <Link to='/projects'>
+      {/* <ul className='flex justify-around ml-auto py-4 md:py-0'> */}
+      {/* <Link to='/projects'>
           <li className='md:px-8'>Projects</li>
         </Link> */}
-        <Link to='/about'>
+      {/* <Link to='/about'>
           <li className='md:px-8'>About Me</li>
-        </Link>
-        {/* <Link to='/projects'>
+        </Link> */}
+      {/* <Link to='/projects'>
           <li className='md:px-8'>Projects</li>
         </Link> */}
-        <Link to='/contact'>
+      {/* <Link to='/contact'>
           <li className='md:px-8'>Contact</li>
-        </Link>
-      </ul>
+        </Link> */}
+      {/* </ul> */}
     </nav>
   )
 }
