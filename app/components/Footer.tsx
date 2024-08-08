1931bf948a80fcb1077ff75778057e4ccc465982
Corey Hayden <cghayden@gmail.com>
2024-08-08 14:00:02 -0400

    turned entire project card into a link

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,7 +3,7 @@ import SocialLinks from './SocialLinks'
 export default function Footer() {
   return (
     <div className='mx-auto w-full flex flex-col pt-4 pb-6 px-4 mt-4'>
-      <div className='flex justify-between w-full max-w-2xl mx-auto'>
+      <div className='flex justify-between w-full max-w-2xl mx-auto items-center'>
         <div>Corey Hayden</div>
         <a href='mailto:cghayden@gmail.com'>cghayden@gmail.com</a>
         <SocialLinks linkedInHandle={'corey-hayden'} />
--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -8,51 +8,51 @@ export default function ProjectCard({ project }: { project: Project }) {
       ? `../project/${project?.slug?.current}/`
       : `project/${project?.slug?.current}`
   return (
-    <li
-      key={project.name}
-      className='w-[350px] sm:w-96 h-64 bg-gray-200 shadow-lg p-3 rounded-lg text-green-950 grid gap-4 grid-rows-projectCard'
-    >
-      <div>
-        <Link to={projectHref}>
+    <Link to={projectHref}>
+      <li
+        key={project.name}
+        className='w-[350px] sm:w-96 h-64 bg-gray-200 shadow-lg p-3 rounded-lg text-green-950 grid gap-4 grid-rows-projectCard'
+      >
+        <div>
           <h2 className='text-2xl text-center text-blue-900 font-semibold'>
             {project.name}
           </h2>
-        </Link>
-      </div>
-      <p className='grid place-content-center leading-relaxed'>
-        {project.shortSummary}
-      </p>
-      <ul className='grid grid-cols-3 gap-2 '>
-        {project.tech?.map((techItem) => (
-          <li key={techItem} className='text-sm italic px-2'>
-            {techItem}
-          </li>
-        ))}
-      </ul>
-      {/* 
+        </div>
+        <p className='grid place-content-center leading-relaxed'>
+          {project.shortSummary}
+        </p>
+        <ul className='grid grid-cols-3 gap-2 '>
+          {project.tech?.map((techItem) => (
+            <li key={techItem} className='text-sm italic px-2'>
+              {techItem}
+            </li>
+          ))}
+        </ul>
+        {/* 
       <div className='flex justify-around flex-wrap'>
-        <Link
-          className=' text-sky-500 py-2 px-2 font-bold flex justify-center'
-          to={`project/${project.slug.current}`}
-        >
-          Tell Me More
-        </Link> */}
-      {/* <Link
+      <Link
+      className=' text-sky-500 py-2 px-2 font-bold flex justify-center'
+      to={`project/${project.slug.current}`}
+      >
+      Tell Me More
+      </Link> */}
+        {/* <Link
           className=' text-sky-500 py-2 px-2 font-bold flex justify-center'
           to={project.githubUrl}
-        >
+          >
           View on Github
-        </Link>
-        {project.liveUrl && (
-          <Link
+          </Link>
+          {project.liveUrl && (
+            <Link
             className=' text-sky-500 py-2 px-2 font-bold flex justify-center'
             to={project.liveUrl}
-          >
+            >
             View me in Action
-          </Link>
-        )} 
-      </div>
-        */}
-    </li>
+            </Link>
+            )} 
+            </div>
+            */}
+      </li>
+    </Link>
   )
 }
